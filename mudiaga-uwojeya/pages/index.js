739a76326c247a4116a96b0e31d602144325d8d9
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-19 15:53:38 +0100

    :lipstick: Add font stylesheet

--- mudiaga-uwojeya/pages/index.js
+++ mudiaga-uwojeya/pages/index.js
@@ -2,7 +2,35 @@ import Head from 'next/head'
 import { Fragment } from 'react';
 
 const Index = () => {
-    return <h1>Hello World</h1>
+    return <Fragment>
+        <Head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="author" content="Mudiaga Moody" />
+            <meta name="description" content="My portfolio website" />
+            <meta
+                name="keywords"
+                content="full stack developer, front end developer
+        back end developer, wewb developer, web development, web design, portfolio website,
+        website design, web designer, website developer, website, web apps, web application Mudiaga Moody Uwojeya Nigeria"
+            />
+            <title>Mudiaga Moody</title>
+
+            <link
+                href="https://fonts.googleapis.com/css2?family=Commissioner:wght@100;200;600&family=Petrona:wght@500;700&family=Quicksand:wght@300&display=swap"
+                rel="stylesheet"
+            />
+
+            /*  external stylesheet and favicon */
+            <link
+                rel="shortcut icon"
+                href="assets/mudi-favicon-sm.png"
+                type="image/x-icon"
+            />
+           
+        </Head>
+        <h1>Hello World</h1>
+    </Fragment>
 }
 
 export default Index;

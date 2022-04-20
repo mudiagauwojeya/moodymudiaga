445d987044289b3200710ed4c89103bbbec9a631
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-20 15:15:44 +0100

    :construction: Import Navigation

--- mudiaga-uwojeya/pages/index.js
+++ mudiaga-uwojeya/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { Fragment } from 'react';
+import Navigation from './components/Navigation';
 import  '../sass/component/index.module.scss';
 
 const Index = () => {

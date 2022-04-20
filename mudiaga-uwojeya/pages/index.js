cc897224adc3532a09bfdd861ca194b884407671
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-20 15:18:46 +0100

    :recycle: Refactor import statement

--- mudiaga-uwojeya/pages/index.js
+++ mudiaga-uwojeya/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { Fragment } from 'react';
 import Navigation from './components/Navigation';
-import  '../sass/component/index.module.scss';
+import  styles from '../sass/component/index.module.scss';
 
 const Index = () => {
     return <Fragment>

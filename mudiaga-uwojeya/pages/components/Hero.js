947db3855da2e0b4b16bcb4da74f86f3ff63b7a9
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-22 20:18:48 +0100

    :recycle: Refactor styles

--- mudiaga-uwojeya/pages/components/Hero.js
+++ mudiaga-uwojeya/pages/components/Hero.js
@@ -4,22 +4,22 @@ import styles from "../../sass/layout/Header.module.scss";
 const Hero = () => {
 
     return <Fragment>
-        <header className="header">
-			<div className="header__bg-video">
-				<video className="header__video" autoPlay muted loop>
+        <header className={styles.header}>
+			<div className={styles.header__bgVideo}>
+				<video className={styles.header__video} autoPlay muted loop>
 					<source src="assets/laptop.mp4" type="video/mp4" />
 					Your current browser does not support video, please upgrade your
 					browser.
 				</video>
 			</div>
-			<div className="header__content">
+			<div className={styles.header__content}>
 				<h1 className="heading-primary">Grow your business</h1>
-				<p className="header__paragraph">
+				<p className={styles.header__paragraph}>
 					get seen by the people looking for your products or services on
 					the internet, drive them to your website and convert them. It is
 					as simple as ...
 				</p>
-				<svg className="header__chevron">
+				<svg className={styles.header__chevron}>
 					<use href="assets/sprite.svg#icon-keyboard_arrow_down"></use>
 				</svg>
 			</div>

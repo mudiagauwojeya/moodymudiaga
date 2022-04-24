f356e93ab4bc7d04c5c13f2fd44a3f4d13bb02a9
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-24 16:36:00 +0100

    :recycle: Refactor styles files

--- mudiaga-uwojeya/pages/components/Navigation.js
+++ mudiaga-uwojeya/pages/components/Navigation.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import styles from '../../sass/layout/Navigation.module.scss'
+import styles from '../../sass/layout/Navigation.module.sass'
 import logo from '../../public/logo.png'
 
 const Navigation = () => {

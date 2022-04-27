572cf8799c4975a710bd0d7edaaf38170e069cb9
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-27 20:44:55 +0100

    :recycle: Refactor Contact

--- mudiaga-uwojeya/pages/components/Contact.js
+++ mudiaga-uwojeya/pages/components/Contact.js
@@ -1,39 +1,40 @@
 import { Fragment } from "react";
+import styles from "../../sass/layout/Contact.module.sass";
 
 const Contact = () => {
     return <Fragment>
-        <section className="contact" id="contact">
-			<form action="mailto:moody.mudiaga@gmail.com" className="contact__form">
-				<label htmlFor="#name" className="contact__label">Name</label>
+        <section className={styles.contact} id="contact">
+			<form action="mailto:moody.mudiaga@gmail.com" className={styles.contact__form}>
+				<label htmlFor="name" className={styles.contact__label}>Name</label>
 				<input
 					type="text"
 					id="name"
-					className="contact__input"
+					className={styles.contact__input}
 					placeholder="Name"
 					required
 				/>
 
-				<label htmlFor="#mail" className="contact__label">Email</label>
+				<label htmlFor="mail" className={styles.contact__label}>Email</label>
 				<input
 					type="email"
 					id="mail"
-					className="contact__input"
+					className={styles.contact__input}
 					placeholder="E-mail"
 					required
 				/>
 
-				<label htmlFor="#message" className="contact__label">Message</label>
+				<label htmlFor="message" className={styles.contact__label}>Message</label>
 				<textarea
 					name="message"
 					id="message"
 					cols="30"
 					rows="15"
-					className="contact__textarea"
+					className={styles.contact__textarea}
 					placeholder="Message"
 					required
 				></textarea>
 
-				<input type="submit" value="Submit" className="contact__button" />
+				<input type="submit" value="Submit" className={styles.contact__button} />
 			</form>
 		</section>
     </Fragment>

0900b59f4e95f33d232758798fa3d2102604df42
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-27 20:24:22 +0100

    :recycle: Refactor testimonial

--- mudiaga-uwojeya/pages/components/Testimonial.js
+++ mudiaga-uwojeya/pages/components/Testimonial.js
@@ -1,22 +1,23 @@
 import { Fragment } from "react";
+import styles from "../../sass/layout/Testimonial.module.sass";
 
 const Testimonial = () => {
     return <Fragment>
-        <section className="testimonial">
-			<div className="testimonial__container">
-				<div className="testimonial__customer">
-					<div className="testimonial__info">
-						<svg className="testimonial__icon">
+        <section className={styles.testimonial}>
+			<div className={styles.testimonial__container}>
+				<div className={styles.testimonial__customer}>
+					<div className={styles.testimonial__info}>
+						<svg className={styles.testimonial__icon}>
 							<use href="assets/sprite.svg#icon-account_circle"></use>
 						</svg>
-						<div className="testimonial__profile">
-							<h4 className="heading-tertiary">Augustine Edward</h4>
-							<p className="testimonial__description">
+						<div className={styles.testimonial__profile}>
+							<h4 className={styles.headingTertiary}>Augustine Edward</h4>
+							<p className={styles.testimonial__description}>
 								CEO - 360 Whiteboard
 							</p>
 						</div>
 					</div>
-					<p className="testimonial__review">
+					<p className={styles.testimonial__review}>
 						Moody is very passionate and all about adding value to what he
 						does. I will never tire to use his services. To anyone
 						wondering whether or not they should hire Moody, you're in
@@ -24,19 +25,19 @@ const Testimonial = () => {
 					</p>
 				</div>
 
-				<div className="testimonial__customer testimonial__customer--1">
-					<div className="testimonial__info testimonial__info--1">
-						<svg className="testimonial__icon">
+				<div className={[styles.testimonial__customer, styles.testimonial__customer1]}>
+					<div className={[styles.testimonial__info, styles.testimonial__info1]}>
+						<svg className={styles.testimonial__icon}>
 							<use href="assets/sprite.svg#icon-account_circle"></use>
 						</svg>
-						<div className="testimonial__profile testimonial__profile--1">
-							<h4 className="heading-tertiary">Naomi Fresh</h4>
-							<p className="testimonial__description">
+						<div className={[styles.testimonial__profile, styles.testimonial__profile1]}>
+							<h4 className={styles.headingTertiary}>Naomi Fresh</h4>
+							<p className={styles.testimonial__description}>
 								Solopreneur - Muna fashion
 							</p>
 						</div>
 					</div>
-					<p className="testimonial__review">
+					<p className={styles.testimonial__review}>
 						Moody makes sure his work is industry standard and
 						satisfactory to his client. I can tell by the efforts he
 						poured into my project. My website is faster than before, and
@@ -44,19 +45,19 @@ const Testimonial = () => {
 					</p>
 				</div>
 
-				<div className="testimonial__customer">
-					<div className="testimonial__info">
-						<svg className="testimonial__icon">
+				<div className={styles.testimonial__customer}>
+					<div className={styles.testimonial__info}>
+						<svg className={styles.testimonial__icon}>
 							<use href="assets/sprite.svg#icon-account_circle"></use>
 						</svg>
-						<div className="testimonial__profile">
-							<h4 className="heading-tertiary">Stewart O. Egedegbe Esq</h4>
-							<p className="testimonial__description">
+						<div className={styles.testimonial__profile}>
+							<h4 className={styles.headingTertiary}>Stewart O. Egedegbe Esq</h4>
+							<p className={styles.testimonial__description}>
 								Principal Partner - S. O. Egedegbe & Co.
 							</p>
 						</div>
 					</div>
-					<p className="testimonial__review">
+					<p className={styles.testimonial__review}>
 						From the design, the performance, and my personal experience
 						using the website Moody designed for us I am very satisfied
 						with the overall result of our project. It was beyond our

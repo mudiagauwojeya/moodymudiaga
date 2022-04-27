050f5c2a7f49cb888d1a1f3f52e394aa6b505f3b
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-27 20:40:35 +0100

    :recycle: Refactor Contact

--- mudiaga-uwojeya/pages/index.js
+++ mudiaga-uwojeya/pages/index.js
@@ -5,6 +5,7 @@ import Hero from './components/Hero';
 import Features from './components/Features';
 import Testimonial from './components/Testimonial';
 import  styles from '../sass/component/Index.module.sass';
+import Contact from './components/Contact';
 
 const Index = () => {
     return <Fragment>
@@ -38,42 +39,7 @@ const Index = () => {
 
 		<Testimonial />
 
-		
-
-		<section className="contact" id="contact">
-			<form action="mailto:moody.mudiaga@gmail.com" className="contact__form">
-				<label htmlFor="#name" className="contact__label">Name</label>
-				<input
-					type="text"
-					id="name"
-					className="contact__input"
-					placeholder="Name"
-					required
-				/>
-
-				<label htmlFor="#mail" className="contact__label">Email</label>
-				<input
-					type="email"
-					id="mail"
-					className="contact__input"
-					placeholder="E-mail"
-					required
-				/>
-
-				<label htmlFor="#message" className="contact__label">Message</label>
-				<textarea
-					name="message"
-					id="message"
-					cols="30"
-					rows="15"
-					className="contact__textarea"
-					placeholder="Message"
-					required
-				></textarea>
-
-				<input type="submit" value="Submit" className="contact__button" />
-			</form>
-		</section>
+		<Contact />
 
 		<footer className="footer">
 			<ul className="footer__list">

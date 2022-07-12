7c9c3f10c2250fe5a4ee0c6259bb19c010a247d5
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-07-12 20:07:03 +0100

    :recycle: Refactor sass to scss syntax

--- portfolio/pages/components/Contact.js
+++ portfolio/pages/components/Contact.js
@@ -1,43 +1,58 @@
-import { Fragment } from "react";
-import styles from "../../sass/layout/Contact.module.sass";
+import { Fragment } from 'react'
+import styles from '../../sass/layout/Contact.module.scss'
 
 const Contact = () => {
-    return <Fragment>
-        <section className={styles.contact} id="contact">
-			<form action="mailto:moody.mudiaga@gmail.com" className={styles.contact__form}>
-				<label htmlFor="name" className={styles.contact__label}>Name</label>
-				<input
-					type="text"
-					id="name"
-					className={styles.contact__input}
-					placeholder="Name"
-					required
-				/>
+  return (
+    <Fragment>
+      <section className={styles.contact} id="contact">
+        <form
+          action="mailto:moody.mudiaga@gmail.com"
+          className={styles.contact__form}
+        >
+          <label htmlFor="name" className={styles.contact__label}>
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            className={styles.contact__input}
+            placeholder="Name"
+            required
+          />
 
-				<label htmlFor="mail" className={styles.contact__label}>Email</label>
-				<input
-					type="email"
-					id="mail"
-					className={styles.contact__input}
-					placeholder="E-mail"
-					required
-				/>
+          <label htmlFor="mail" className={styles.contact__label}>
+            Email
+          </label>
+          <input
+            type="email"
+            id="mail"
+            className={styles.contact__input}
+            placeholder="E-mail"
+            required
+          />
 
-				<label htmlFor="message" className={styles.contact__label}>Message</label>
-				<textarea
-					name="message"
-					id="message"
-					cols="30"
-					rows="15"
-					className={styles.contact__textarea}
-					placeholder="Message"
-					required
-				></textarea>
+          <label htmlFor="message" className={styles.contact__label}>
+            Message
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            cols="30"
+            rows="15"
+            className={styles.contact__textarea}
+            placeholder="Message"
+            required
+          ></textarea>
 
-				<input type="submit" value="Submit" className={styles.contact__button} />
-			</form>
-		</section>
+          <input
+            type="submit"
+            value="Submit"
+            className={styles.contact__button}
+          />
+        </form>
+      </section>
     </Fragment>
+  )
 }
 
-export default Contact;
+export default Contact
